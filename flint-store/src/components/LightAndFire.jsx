8deb967e3268378e9fd91518d6dd0fdf3ebd19a3
Emilie Anglade <anglade.em@gmail.com>
2020-11-19 21:23:13 +0100

    display sub categories + cards items

--- flint-store/src/components/LightAndFire.jsx
+++ flint-store/src/components/LightAndFire.jsx
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DisplaySubCategories from './displaySubCategories/DisplaySubCategories';
 import JsonFAke from './../static/JsonFAke.json';
 
 const LightAndFire = () => {
   
-  
-  const alumette = JsonFAke.filter(items => items.type === "Alumette")
+  const allumette = JsonFAke.filter(items => items.type === "Allumette");
+  const briquet = JsonFAke.filter(items => items.type === "Briquet");
+  const torche = JsonFAke.filter(items => items.type === "Torche");
+  const barbecue = JsonFAke.filter(items => items.type === "Barbecue");
+
+
 
   return (
     <>
 
       <h1>Light and Fire page</h1>
       
-      <DisplaySubCategories type={alumette}/>
+      <DisplaySubCategories type={allumette} title={allumette[0].type} />
+      <DisplaySubCategories type={briquet} title={briquet[0].type}/>
+      <DisplaySubCategories type={torche} title={torche[0].type}/>
+      <DisplaySubCategories type={barbecue} title={barbecue[0].type}/>
     </>
   );
 }
--- flint-store/src/components/displaySubCategories/DisplaySubCategories.jsx
+++ flint-store/src/components/displaySubCategories/DisplaySubCategories.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import CardItems from './CardItems';
 
-const DisplaySubCategories = ({type}) => {
+const DisplaySubCategories = ({type, title}) => {
   
   return (
      <div>
       <p>DisplaySubCategories page</p>
       <div>
+        <h1>{title}</h1>
         { type.map(item => {
           return (
-            <CardItems 
-              key={item.item_id}
-              props ={item}
-          />
+            <div>
+              <CardItems 
+                key={item.item_id}
+                props ={item}
+              />
+            </div>
           )
         })}
       </div>
