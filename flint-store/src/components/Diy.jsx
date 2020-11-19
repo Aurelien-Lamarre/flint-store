baeb6456b5688538812ebafad3386afb62d9ebc9
Aurelien <lamarreaurelien@outlook.fr>
2020-11-19 16:04:07 +0100

    I made the four main components and their corresponding route

---
+++ flint-store/src/components/Diy.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Diy = () => {
+  return (
+    <p>DIY page</p>
+  );
+}
+
+export default Diy;
---
+++ flint-store/src/components/TorchParty.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const TorchParty = () => {
+  return (
+    <p>Torch Party page</p>
+  );
+}
+
+export default TorchParty;
