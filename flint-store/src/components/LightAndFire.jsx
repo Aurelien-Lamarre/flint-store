faeb8eaade305962d7eb5b85260b7a960127f780
Aurelien <lamarreaurelien@outlook.fr>
2020-11-19 19:49:18 +0100

    Basic filter and map of items

--- flint-store/src/components/LightAndFire.jsx
+++ flint-store/src/components/LightAndFire.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import DisplaySubCategories from './displaySubCategories/DisplaySubCategories';
+import JsonFAke from './../static/JsonFAke.json';
+import CardItems from './displaySubCategories/CardItems';
 
 const LightAndFire = () => {
+  const [type, setType] = useState([]);
+  useEffect(() => {
+    const { url } = './../static/JsonFAke.json'
+    axios.get(url)
+      .then(res => res.data)
+      .then(data => {
+        console.log(data)
+        setType(data)
+
+      })
+  })
+  
+const alumette = JsonFAke.filter(items => items.type === "Alumette")
+
   return (
     <>
       <h1>Light and Fire page</h1>
+      <DisplaySubCategories props={alumette }/>
+      {/* <DisplaySubCategories />
       <DisplaySubCategories />
-      <DisplaySubCategories />
-      <DisplaySubCategories />
-      <DisplaySubCategories />
+      <DisplaySubCategories /> */}
     </>
   );
 }
 
 export default LightAndFire;
+
+
+
+// JsonFAke.filter(el => el.category === "Feu et Lumiere").map(item => {
+//   <p>{item.title }</p>
+//   })
--- flint-store/src/components/displaySubCategories/DisplaySubCategories.jsx
+++ flint-store/src/components/displaySubCategories/DisplaySubCategories.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import CardItems from './CardItems';
+import JsonFake from './../../static/JsonFAke.json';
 
-const DisplaySubCategories = () => {
+const DisplaySubCategories = ({props}) => {
+  // const [list, setList] = useEffect([]);
+  
   return (
-    <>
-      <p>DisplaySubCategories page</p>
-      <CardItems />
+
+  <>
+    <p>Type</p>
+      {
+        props.map(el => {
+          return (
+            <CardItems key={el.item_id} props={el} />
+          )
+        })
+    }
     </>
-  );
+  )
+  ;
 }
 
 export default DisplaySubCategories;
