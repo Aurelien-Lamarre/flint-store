0fb72c3b19f16d593183e6b2368729f2206c1158
Emilie Anglade <anglade.em@gmail.com>
2020-11-19 23:33:04 +0100

    first-navbar

---
+++ flint-store/src/components/Navigation.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'; 
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Navigation = (props) => {
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarToggler onClick={toggleNavbar} className="mr-4" />
+        <NavbarBrand href="/" className="mr-4">Flint Store</NavbarBrand>
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem className="mr-4">
+              <NavLink href="/lightandfire" className="nav-link">Fire and Light</NavLink>
+            </NavItem>
+            <NavItem className="mr-4">
+              <NavLink href="/diy">D.I.Y.</NavLink>
+            </NavItem>
+            <NavItem className="mr-4">
+              <NavLink href="/weaponsandhunting">Weapons</NavLink>
+            </NavItem>
+            <NavItem className="mr-4">
+              <NavLink href="/torchparty">Torch Party</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Navigation;
