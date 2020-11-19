e3fd0c17d5534cd4f9512152e60f594e1eb27199
FabGtte <41702805+FabGtte@users.noreply.github.com>
2020-11-19 23:24:13 +0100

    Update SQLQueryFlintStore.sql

--- flint-store-backend/BDDSQL/SQLQueryFlintStore.sql
+++ flint-store-backend/BDDSQL/SQLQueryFlintStore.sql
@@ -8,7 +8,7 @@ GO
 CREATE TABLE SubCategory (
 subCategoryId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 subCategoryName VARCHAR(100) NOT NULL,
-FK_CategoryId INT FOREIGN KEY REFERENCES [Category](categoryId) NOT NULL
+FK_categoryId INT FOREIGN KEY REFERENCES [Category](categoryId) NOT NULL
 );
 GO
 CREATE TABLE Item (
@@ -17,6 +17,6 @@ itemName VARCHAR(100) NOT NULL,
 itemDescription VARCHAR(300),
 itemPrice FLOAT,
 itemImg NVARCHAR(500), 
-subCategoryId INT FOREIGN KEY REFERENCES [SubCategory](subCategoryId) NOT NULL,
+FK_subCategoryId INT FOREIGN KEY REFERENCES [SubCategory](subCategoryId) NOT NULL,
 FK_categoryId INT FOREIGN KEY REFERENCES [Category](categoryId) NOT NULL,
 );
