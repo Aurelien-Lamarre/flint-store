fdfd8eadaa31782450d6b25132ab70209c7581b5
FabGtte <fabiengilliotte@gmail.com>
2020-11-19 18:16:05 +0100

    Create BDD

---
+++ flint-store-backend/BDDSQL/SQLQueryFlintStore.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE flintStore 
+GO
+CREATE TABLE Category (
+categoryId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+categoryName VARCHAR(100) NOT NULL, 
+);
+GO
+CREATE TABLE SubCategory (
+subCategoryId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+subCategoryName VARCHAR(100) NOT NULL,
+FK_CategoryId INT FOREIGN KEY REFERENCES [Category](categoryId) NOT NULL
+);
+GO
+CREATE TABLE Item (
+itemId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+itemName VARCHAR(100) NOT NULL,
+itemDescription VARCHAR(300),
+itemPrice FLOAT,
+itemImg NVARCHAR(500), 
+FK_typeId INT FOREIGN KEY REFERENCES [SubCategory](subCategoryId) NOT NULL,
+FK_CategoryId INT FOREIGN KEY REFERENCES [Category](categoryId) NOT NULL,
+);
