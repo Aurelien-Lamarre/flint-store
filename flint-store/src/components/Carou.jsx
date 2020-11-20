3060f0a53eb3270b0d0fd1d31c01fd64631c9304
samson-corbiere <samson.corbiere@gmail.com>
2020-11-20 13:51:54 +0100

    carousel is done

--- flint-store/src/components/Carou.jsx
+++ flint-store/src/components/Carou.jsx
@@ -12,17 +12,17 @@ const items = [
   {
     src: 'https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2020-07/rr.jpg',
     altText: 'Slide 1',
-    caption: 'IL VA FAIRE TOUT NOIR !!!'
+    caption: 'ÇA VA ETRE TOUT NOIR !!!'
   },
   {
     src: 'https://www.rokai.fr/wp-content/uploads/2019/03/enrochements-3-provinces-e1553522422804.jpg',
     altText: 'Slide 2',
-    caption: 'THESE STONES ARE BEAUTIFUL'
+    caption: 'BIG DISCOUNT ON OUR UTILITY STONES'
   },
   {
     src: 'https://i.pinimg.com/originals/1e/6c/bc/1e6cbc954124bfd6146e0f15b28d9456.jpg',
     altText: 'Slide 3',
-    caption: 'WOOOAAAAAAW'
+    caption: 'DISCOVER OUR NEW WALLPAPER VERY SOON'
   }
 ];
 
