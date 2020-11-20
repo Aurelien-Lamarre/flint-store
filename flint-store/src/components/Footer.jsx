ed44442273f5e572a215f85d26cb94096abd456e
samson-corbiere <samson.corbiere@gmail.com>
2020-11-20 09:35:30 +0100

    add footer with white line in the bottom

--- flint-store/src/components/Footer.jsx
+++ flint-store/src/components/Footer.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Styled from 'styled-components';
-import './Footer.css'
- 
-const FooterWrap = Styled.div`
-  display:flex;
-  flex-direction: column;
-  height: 10em;
-  width:100%;
-  background: slategrey;
-  `;
 
-const Footer = () => {
+import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import styled from 'styled-components'
+import "./Footer.css"
+
+const NavFooter = styled.div`
+  display: flex;
+  background-color: #343a40;
+  font-size: 1em;
+  align-items: center;
+  flex-wrap: wrap;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  margin-top: 1em
+`;
+
+const NavItemFooter = styled.div`
+color: white;
+text-decoration: none
+margin right: 1em
+`;
+
+const NaviLink = styled.link`
+color: white;
+text-decoration: none
+margin right: 1em
+margin top : 1em
+padding top: 10em 
+`;
+
+const Footer = (props) => {
   return (
-    <FooterWrap>
-      <Link className='footerlink' to='/privacy'>Privacy</Link>
-      <Link className='footerlink' to='/faq'>FAQ</Link>
-      <Link className='footerlink' to='/terms'>Terms</Link>
-    </FooterWrap>
+    <div>
+      <NavFooter className="NavItemFooterCss">
+        <NavItemFooter>
+          <NavLink className="NavItemLink" href="/privacy">Privacy</NavLink>
+        </NavItemFooter>
+        <NavItemFooter>
+          <NavLink className="NavItemLink" href="/faq">F.A.Q</NavLink>
+        </NavItemFooter>
+        <NavItemFooter>
+          <NavLink className="NavItemLink" href="/terms">Terms</NavLink>
+        </NavItemFooter>
+      </NavFooter>
+      <hr />
+    </div>
   );
 }
 
 export default Footer;
+
